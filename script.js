6762c1599052e74a194999dff45ddf28cc0dc07e
Vadin322 <pozarvadim0@gmail.com>
2020-02-23 00:11:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    $('.slid').slick({  
+     arrows: false,
+     dots: true
+});
+});
+$(document).ready(function(){
+    $('.slid2').slick({  
+        dots: true, 
+        arrows: true, 
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3   
+});
+});
